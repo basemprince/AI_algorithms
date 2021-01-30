43519e5c49f19763cf0ea3ddfdf78a84ad95790e
basemprince <bshaker@kth.se>
2021-01-30 17:06:54 +0100

    before iterative

--- minimax_assignment/player.py
+++ minimax_assignment/player.py
@@ -6,7 +6,6 @@ from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
 
-
 class PlayerControllerHuman(PlayerController):
     def player_loop(self):
         """
@@ -71,7 +70,8 @@ class PlayerControllerMinimax(PlayerController):
 
         Please note that the number of fishes and their types is not fixed between test cases.
         """
-        model = minmax_algorithm(initial_data)
+        subdivisions = self.settings.space_subdivisions
+        model = minmax_algorithm(initial_data, subdivisions)
 
         return model
 
@@ -91,7 +91,7 @@ class PlayerControllerMinimax(PlayerController):
         
         # NOTE: Don't forget to initialize the children of the current node 
         #       with its compute_and_get_children() method!
-
+        
         start_time = time()
         best_possible_move,_= model.minmax_prune(initial_tree_node,start_time)
 
@@ -99,7 +99,7 @@ class PlayerControllerMinimax(PlayerController):
 
 
 class minmax_algorithm:
-    def __init__(self, initial_data):
+    def __init__(self, initial_data,subdivisions):
         self.initial_data = initial_data
         self.next_children = []
         self.bestPossibleMove = 0
@@ -107,6 +107,7 @@ class minmax_algorithm:
         self.type = 'combination'
         self.target = None
         self.y_check = 0
+        self.subdivisions = subdivisions
 
     def hursitic(self,node):
         if self.type == 'score':
@@ -119,7 +120,6 @@ class minmax_algorithm:
 
             my_hook = node.state.hook_positions[0]
             oponent_hook = node.state.hook_positions[1]
-            print(my_hook)
             score_diff = node.state.player_scores[0]-node.state.player_scores[1]
             self.y_check = 0
             closest = my_distance = float('inf')
@@ -128,23 +128,27 @@ class minmax_algorithm:
                 fish = fish_list[key]
                 fish_s = fish_score[key]
 
-                my_distance = ((my_hook[0] - fish[0])**2 + (my_hook[1] - fish [1])**2)
+                my_distance1 = ((my_hook[0] - fish[0])**2 + (my_hook[1] - fish [1])**2)
+                my_distance2 = ((my_hook[0] - fish[0] + self.subdivisions)**2 + (my_hook[1] - fish [1])**2)
+                my_distance3 = ((my_hook[0] - fish[0] - self.subdivisions)**2 + (my_hook[1] - fish [1])**2)
+                my_distance = min(my_distance1,my_distance2,my_distance3)
+
                 oponent_distance = math.sqrt((oponent_hook[0] - fish[0])**2 + (oponent_hook[1] - fish [1])**2)
 
                 #check if fish is worth going after, if this fish is the closest, and oponent is not within 1 block
                 if fish_s >= 0 and caught_check != key and my_distance < closest and oponent_distance >=1:
-                    self.target = fish_s
+                    self.target = key
                     closest = my_distance                 
 
                 #check how many fish are on other side
                 if fish[0] > oponent_hook[0] and oponent_hook[0] > my_hook[0]:
                     fish_away_count += 1
-                if fish[1] < my_hook[1]:
+                if fish[1] > my_hook[1]:
                     self.y_check += 1
 
             percent_away = fish_away_count / len(fish_list) if len(fish_list) !=0 else 1
 
-            # move ship to the other side if remaning fish are there
+            #move ship to the other side if remaning fish are there
             if percent_away >= 0.99 and node.move == 3:
                 closest = float('-inf')
             try:
@@ -161,7 +165,7 @@ class minmax_algorithm:
             illegal = True
         elif Node.state.hook_positions[0][0] - Node.state.hook_positions[1][0] == 1 and Node.move == 3:
             illegal = True
-        elif self.y_check > 0 and Node.move == 1:
+        elif self.y_check == 0 and Node.move == 1:
             illegal = True
         return illegal
 
@@ -170,9 +174,9 @@ class minmax_algorithm:
 
         self.next_children = CurrentNode.compute_and_get_children()
 
-        if self.next_children == []   or elapsed_time >= 10*1e-3: #
+        if self.next_children == []   or elapsed_time >= 8*1e-3: #
             huristic = self.hursitic(CurrentNode)
-            #print("hurisitic:" ,huristic, "depth:", CurrentNode.depth, "move:", CurrentNode.move)
+            print("hurisitic:" ,huristic, "depth:", CurrentNode.depth, "move:",  ACTION_TO_STR[CurrentNode.move])
             return  CurrentNode.move, huristic
 
         else:
@@ -180,7 +184,7 @@ class minmax_algorithm:
             bestPossible = float('-inf') if current_player == 0 else float ('inf')
 
             for child in self.next_children:
-               # print (child.state.hook_positions[0][0] , "   " ,child.state.hook_positions[1][0])
+                #print (child.state.hook_positions[0][0] , "   " ,child.state.hook_positions[1][0])
                 if self.illegal_moves(child):
                     #print("whatsup")
                     continue
