ee4f8c6409a37886527741053aece21f294639af
basemprince <bshaker@kth.se>
2021-02-15 19:19:35 +0100

    started hmm2

--- hmm_sk/hmm2.py
+++ hmm_sk/hmm2.py
@@ -2,11 +2,12 @@
 
 """
 Author: Basem Shaker
-Assignment: HMM1
+Assignment: HMM2
 """
 
 import sys
 import argparse
+from math import log as log
 
 
 def parse(arguments):
@@ -78,12 +79,57 @@ def alphaPass(emissionSequence, transMatrix, emissionMatrix, initialState):
 
     return alphaMatrix
 
+def matrixLog(vector):
+    print(type(vector))
+    for i in range(len(vector)):
+        if(vector[i])!=0:
+            vector[i]= log(vector[i])
+    return vector
+
+
+# V = sequence
+# a = transmission
+# b = emission probability
+def viterbiAlg(emissionSequence, transMatrix, emissionMatrix, initialState):
+    rows = len(emissionSequence[0])
+    columns = len(transMatrix)
+    omegaMatrix = [0] * rows * columns
+    omegaMatrix = matrixCreator(omegaMatrix,rows,columns)
+    prevMatrix = [0] * (rows-1) * columns
+    prevMatrix = matrixCreator(prevMatrix,rows-1,columns)
+
+    omega1 = vectorMultiply(initialState[0] , list(zip(*emissionMatrix))[emissionSequence[0][0]])
+    omega1 = matrixLog(omega1)
+
+    for i in range(len(omegaMatrix[0])):
+        omegaMatrix[0][i]= omega1[i]
+
+    # for t in range(1, len(emissionSequence[0])):
+    #     for j in range(len(transMatrix)):
+    #         dotProd =  dotProduct(alphaMatrix[t - 1],list(zip(*transMatrix))[j])
+    #         alphaMatrix[t][j] = dotProd * emissionMatrix[j][emissionSequence[0][t]]
+
+
+    for t in range(1, rows):
+        for j in range(columns):
+            list(zip(*emissionMatrix))[emissionSequence[0][0]])
+            # probability = omegaMatrix[t-1]  + log(emissionMatrix[j][emissionSequence[0][t]])
+            
+            # probability = omega[t - 1] + np.log(a[:, j]) + np.log(b[j, V[t]])
+ 
+            # # This is our most probable state given previous state at time t (1)
+            # prev[t - 1, j] = np.argmax(probability)
+ 
+            # # This is the probability of the most probable state (2)
+            # omega[t, j] = np.max(probability)
+
+
+
+
+
+    return omegaMatrix
+
 
 transMatrix, emissionMatrix, initialState , emissionSequence = parse(sys.argv[1:])
-alphaMatrix = alphaPass(emissionSequence,transMatrix,emissionMatrix,initialState)
+LikelySeq = viterbiAlg(emissionSequence,transMatrix,emissionMatrix,initialState)
 
-probOfSeq = 0
-for element in alphaMatrix[-1]:
-    probOfSeq+=element
-
-print(probOfSeq)
