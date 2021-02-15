13af34b12727af554b75be1063d715249aecba68
basemprince <bshaker@kth.se>
2021-02-15 23:33:56 +0100

    hmm3 progress

--- hmm_sk/hmm3.py
+++ hmm_sk/hmm3.py
@@ -8,6 +8,7 @@ Assignment: HMM3
 import sys
 import argparse
 from math import log as log
+import numpy as np
 
 def parse(arguments):
 
@@ -33,6 +34,16 @@ def parse(arguments):
 
     return inputArray[0] , inputArray[1] , inputArray[2] , inputArray[3]
 
+def outputParser(inputMatrix):
+    rows = len(inputMatrix)
+    columns = len(inputMatrix[0])
+    parsedList = [rows,columns]
+
+    for i in range(rows):
+        for j in range(columns):
+            parsedList.append(inputMatrix[i][j])
+    return parsedList
+
 def matrixCreator(array,rows,columns,intCheck=False):
     matrix = []
     for i in range(rows):
@@ -46,19 +57,66 @@ def matrixCreator(array,rows,columns,intCheck=False):
 
     return matrix
 
+def matrixCreator3D(array,rows,columns,elements,intCheck=False):
+    matrix = []
+    for i in range(rows):
+        rowArray = []
+        for j in range(columns):
+            internalArray = []
+            for k in range(elements):
+                if intCheck:
+                    internalArray.append(int(array[columns * i + j+k]))
+                else:
+                    internalArray.append(float(array[columns * i + j+ k])) 
+            rowArray.append(internalArray)               
+        matrix.append(rowArray)
+
+    return matrix
+
+
 def dotProduct(vector1, vector2):
     return sum(x*y for x,y in zip(vector1,vector2))
+
+def dotProduct2(vector1, vector2):
+    rows = len(vector1)
+    columns = len(vector2[0])
+    result = [0] * rows * columns
+    result = matrixCreator(result,rows,columns)
+    for r in range(rows):
+        for c in range(columns):
+            total = 0
+            for i in range(columns):
+                total += vector1[r][i] * vector2[i][c]       
+            result[r][c] = total  
+    return result
 
 def vectorMultiply(vector1,vector2):
     result = []
     if(type(vector2) == float):
         for x in vector1:
             result.append(x*vector2)
+    elif(type(vector1) == float):
+        for x in vector2:
+            result.append(x*vector1)
     else:
         for x,y in zip(vector1,vector2):
             result.append(x*y) 
     return result
     
+
+def vectorDivide(vector1,vector2):
+    result = []
+    if(type(vector2) == float):
+        for x in vector1:
+            result.append(x/vector2)
+    elif(type(vector1) == float):
+        for x in vector2:
+            result.append(x/vector1)
+    else:
+        for x,y in zip(vector1,vector2):
+            result.append(x/y) 
+    return result
+
 
 def matrixLog(vector):
 
@@ -75,6 +133,7 @@ def matrixAddition(vector1,vector2):
     if(type(vector2) == float):
         for x in vector1:
             result.append(x+vector2)
+
     else:
         for x,y in zip(vector1,vector2):
             result.append(x+y) 
@@ -102,73 +161,56 @@ def betaPass(emissionSequence, transMatrix, emissionMatrix):
     columns = len(transMatrix)
     betaMatrix = ([0] * (rows-1) * columns) + ([1]* columns)
     betaMatrix = matrixCreator(betaMatrix,rows,columns)
-    print(betaMatrix)
-    beta1 = vectorMultiply(initialState[0] , list(zip(*emissionMatrix))[emissionSequence[0][0]])
 
-    # for i in range(len(alphaMatrix[0])):
-    #     alphaMatrix[0][i]= alpha1[i]
-
-    # for t in range(1, len(emissionSequence[0])):
-    #     for j in range(len(transMatrix)):
-    #         dotProd =  dotProduct(alphaMatrix[t - 1],list(zip(*transMatrix))[j])
-    #         alphaMatrix[t][j] = dotProd * emissionMatrix[j][emissionSequence[0][t]]
+    for t in range(rows - 2, -1, -1):
+        for j in range(columns):
+            multiplicationStep = vectorMultiply(betaMatrix[t+1],list(zip(*emissionMatrix))[emissionSequence[0][t+1]])
+            betaMatrix[t][j] = dotProduct(multiplicationStep , transMatrix[j])
 
     return betaMatrix
 
 
 
-def viterbiAlg(emissionSequence, transMatrix, emissionMatrix, initialState):
-    rows = len(emissionSequence[0])
-    columns = len(transMatrix)
+def bwAlgorithm(emissionSequence, transMatrix, emissionMatrix, initialState,iter=100):
 
-    omegaMatrix = [0] * rows * columns
-    omegaMatrix = matrixCreator(omegaMatrix,rows,columns)
+    M = len(transMatrix)
+    T = len(emissionSequence[0])
 
-    prevMatrix = [0] * (rows-1) * columns
-    prevMatrix = matrixCreator(prevMatrix,rows-1,columns)
+    for curr_iter in range(iter):
+        alphaMatrix = alphaPass(emissionSequence,transMatrix,emissionMatrix,initialState)
+        betaMatrix  = betaPass(emissionSequence,transMatrix,emissionMatrix)
 
-    likelySeq = [0] * rows
+        xi = ([0] * M * M)* (T-1)
+        xi = matrixCreator3D(xi,M,M,T-1)
+        
+        for t in range(T - 1):
+            
+            dotCalc1 = dotProduct2([list(list(zip(*alphaMatrix))[t])] , transMatrix)[0]
 
-    omega1 = vectorMultiply(initialState[0] , list(zip(*emissionMatrix))[emissionSequence[0][0]])
-    omega1 = matrixLog(omega1)
+            b_temp = list(zip(*emissionMatrix))
+            b_trans=list((list(zip(*b_temp))[emissionSequence[0][t+1]]))
 
-    for i in range(len(omegaMatrix[0])):
-        omegaMatrix[0][i]= omega1[i]
+            multCalc = vectorMultiply(dotCalc1,b_trans)
+            denominator = dotProduct(multCalc,betaMatrix[t+1])
+            for i in range(M):
+                mult1 = vectorMultiply( alphaMatrix[t][i],transMatrix[i])
+                mult2 = vectorMultiply( mult1,b_trans)
+                numerator = vectorMultiply( mult2,list(zip(*betaMatrix))[t+1])    
+                for k in range(len(xi[i])):           
+                    xi[i][k][t] = vectorDivide(numerator, denominator)
 
-    for t in range(1, rows):
-        for j in range(columns):
-            log1= matrixLog(list(list(zip(*transMatrix))[j]))
-            try:
-                log2=log(emissionMatrix[j][emissionSequence[0][t]])
-            except:
-                log2 = float('-inf')
-            probability = matrixAddition(matrixAddition(omegaMatrix[t-1],log1),log2)     
-            maxIndex = probability.index(max(probability))
+        
+        gamma = sum(xi)
+        print(gamma)
+    # print(xi)
 
-            prevMatrix[t-1][j] = maxIndex
-            omegaMatrix[t][j] = max(probability)
-            # print(probability , 'max ' , maxIndex)
-    
-    latestState = omegaMatrix[rows-1].index(max(omegaMatrix[rows-1]))
-    # print(latestState)
-
-    likelySeq[0] = latestState
- 
-    index = 1
-    for i in range(rows - 2, -1, -1):
-        likelySeq[index] = prevMatrix[i][latestState]
-        latestState = prevMatrix[i][latestState]
-        index += 1
-
-    likelySeq = likelySeq[::-1]
-
-    return likelySeq
+    return transMatrix , emissionMatrix
 
 
 transMatrix, emissionMatrix, initialState , emissionSequence = parse(sys.argv[1:])
-alphaMatrix = alphaPass(emissionSequence,transMatrix,emissionMatrix,initialState)
-betaMatrix  = betaPass(emissionSequence,transMatrix,emissionMatrix)
-likelySeq = viterbiAlg(emissionSequence,transMatrix,emissionMatrix,initialState)
+transMatrix , emissionMatrix = bwAlgorithm(emissionSequence, transMatrix, emissionMatrix, initialState)
+
+
 
 # print(' '.join(map(str,likelySeq)))
 
